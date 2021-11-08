d2758ed751f4866f65283cdfeee5b8182d58c1b2
YS <yoonsam86@gmail.com>
2021-11-08 22:06:53 +0900

    [W2-1 Layouts Codelab] Create your custom layout

--- week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/ys/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/ys/layoutscodelab/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -28,9 +29,14 @@ import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontWeight.Companion
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.ys.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
@@ -42,6 +48,75 @@ class MainActivity : ComponentActivity() {
                 LayoutsCodelab()
             }
         }
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        // 자식 View를 더 이상 제한하지 말고 주어진 제약 조건으로 측정합니다.
+        // 측정된 자식 목록입니다.
+        val placeables = measurables.map { measurable ->
+            // 각각 자식을 측정
+            measurable.measure(constraints)
+        }
+
+        // 자식을 배치한 y 좌표를 추적
+        var yPosition = 0
+
+        // 레이아웃의 크기를 최대한 크게 설정
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // 화면에서 항목 위치 지정
+            placeables.forEach { placeable ->
+                // 화면에서 항목 위치 지정
+                placeable.placeRelative(x = 0, y = yPosition)
+
+                // 배치된 y 좌표를 기록합니다.
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            // placeRelative는 현재 layoutDirection을 기반으로 배치 가능한 위치를 자동으로 조정합니다.
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 32.dp))
     }
 }
 
@@ -61,15 +136,27 @@ fun LayoutsCodelab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+        BodyContent(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.padding(top = 10.dp)
+        ) {
+            Text(text = "button")
+        }
     }
 }
 
@@ -111,11 +198,6 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
             }
         }
     }
-}
-
-@Composable
-fun Button() {
-
 }
 
 @Preview
